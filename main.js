656735ee7a34c85c78cee412d5da429118292d63
olawale <jao.olaniyi@gmail.com>
2022-12-17 11:11:59 +0100

    jonas challenge 1-2

--- main.js
+++ main.js
@@ -1,51 +1,72 @@
-alert("WELCOME TO DIGITAL LIBRARY");
-let books = prompt('Title of the book');
+// alert("WELCOME TO DIGITAL LIBRARY");
+// let books = prompt('Title of the book');
 
-let author = prompt('Name of the author');
+// let author = prompt('Name of the author');
 
-let publisher = prompt('Name of the publisher');
+// let publisher = prompt('Name of the publisher');
 
-let bOoks = prompt('Title of the book');
+// let bOoks = prompt('Title of the book');
 
-let aUthor = prompt('Name of the author');
+// let aUthor = prompt('Name of the author');
 
-let pUblisher = prompt('Name of the publisher');
+// let pUblisher = prompt('Name of the publisher');
 
-let boOks = prompt('Title of the book');
+// let boOks = prompt('Title of the book');
 
-let auThor = prompt('Name of the author');
+// let auThor = prompt('Name of the author');
 
-let puBlisher = prompt('Name of the publisher');
+// let puBlisher = prompt('Name of the publisher');
 
-book1.innerHTML=books;
-author1.innerHTML=author;
-publisher1.innerHTML=publisher;
-book2.innerHTML=bOoks;
-author2.innerHTML=aUthor;
-publisher2.innerHTML=pUblisher;
-book3.innerHTML=boOks;
-author3.innerHTML=auThor;
-publisher3.innerHTML=puBlisher;
+// book01.innerHTML=books;
+// author1.innerHTML=author;
+// publisher1.innerHTML=publisher;
+// book2.innerHTML=bOoks;
+// author2.innerHTML=aUthor;
+// publisher2.innerHTML=pUblisher;
+// book3.innerHTML=boOks;
+// author3.innerHTML=auThor;
+// publisher3.innerHTML=puBlisher;
 
-const data  = {
+// const data  = {
 
-    "Author1": {
-        "book": books,
-        "author": author,
-         "publisher": publisher,
-    },
+//     "Author1": {
+//         "book": books,
+//         "author": author,
+//          "publisher": publisher,
+//     },
 
-    "Author2": {
-        "book": bOoks,
-        "author": aUthor,
-        "publisher": pUblisher,
-    },
-    "Author3": {
-        "book": boOks,
-        "author": auThor,
-        "publisher": puBlisher,
-    }
+//     "Author2": {
+//         "book": bOoks,
+//         "author": aUthor,
+//         "publisher": pUblisher,
+//     },
+//     "Author3": {
+//         "book": boOks,
+//         "author": auThor,
+//         "publisher": puBlisher,
+//     }
 
-}
+// }
 
-console.log(data);
+// console.log(data);
+
+// JONAS EXERCISES
+// Assignment One
+// BMI = mass / height ** 2 or mass / (height * height)
+let johnMass =92;
+let johnHeight = 1.95;
+let markMass =78;
+let markHeight = 1.69;
+
+ let johnBMI = johnMass / johnHeight **2
+ console.log(johnBMI)
+ let markBMI = markMass / (markHeight *markHeight )
+ console.log(markBMI)
+ console.log(markBMI > johnBMI)
+
+ if(markBMI > johnBMI){
+console.log(`markbmi (${markBMI}) is greater the johnbmi (${johnBMI}) `)
+ }else{
+     console.log(`markbmi (${markBMI}) is lesser the johnbmi (${johnBMI}) `)
+
+ }
