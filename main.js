4d7fadab26e98920b712e266fe7070b684b3f77e
olawale <jao.olaniyi@gmail.com>
2022-12-20 12:21:15 +0100

    js

--- main.js
+++ main.js
@@ -53,20 +53,124 @@
 // JONAS EXERCISES
 // Assignment One
 // BMI = mass / height ** 2 or mass / (height * height)
-let johnMass =92;
+let johnMass = 92;
 let johnHeight = 1.95;
-let markMass =78;
+let markMass = 78;
 let markHeight = 1.69;
 
- let johnBMI = johnMass / johnHeight **2
- console.log(johnBMI)
- let markBMI = markMass / (markHeight *markHeight )
- console.log(markBMI)
- console.log(markBMI > johnBMI)
+let johnBMI = johnMass / johnHeight ** 2;
+console.log(johnBMI);
+let markBMI = markMass / (markHeight * markHeight);
+console.log(markBMI);
+console.log(markBMI > johnBMI);
 
- if(markBMI > johnBMI){
-console.log(`markbmi (${markBMI}) is greater the johnbmi (${johnBMI}) `)
- }else{
-     console.log(`markbmi (${markBMI}) is lesser the johnbmi (${johnBMI}) `)
+if (markBMI > johnBMI) {
+  console.log(`markbmi (${markBMI}) is greater the johnbmi (${johnBMI}) `);
+} else {
+  console.log(`markbmi (${markBMI}) is lesser the johnbmi (${johnBMI}) `);
+}
 
- }
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+john.calcBMI();
+mark.calcBMI();
+console.log(
+  `${john.fullName}'s BMI ${john.bmi}kg is lesser than ${mark.fullName}'s BMI ${mark.bmi}kg`
+);
+console.log(john.fullName);
+console.log(john.bmi);
+
+//  const teamDolphins = 96 + 108 + 89
+//  const teamKaolas =  88 + 91 + 110
+//  const averageScoreD = teamDolphins / 3
+//   console.log(averageScoreD)
+//  const averageScoreK = teamKaolas /3
+//   console.log(averageScoreK)
+
+//   if(averageScoreD > averageScoreK){
+// console.log('teamDolphins is the winner')
+//   }else{
+//     console.log('teamKaolas is the winner')
+//   }
+
+// const teamDolphins = 97 + 112 + 102;
+// const teamKaolas = 109 + 95 + 123;
+// const averageScoreD = teamDolphins / 3;
+// const averageScoreK = teamKaolas / 3;
+// const minScore = 100
+
+// if(averageScoreD < minScore){
+// console.log('team dolphins wins')
+// }
+// if (averageScoreK > minScore){
+//     console.log('team kaolas wins')
+// }
+// if ( averageScoreD === averageScoreK){
+//     console.log('is it a draw')
+// }
+
+// const dolphins = 44 + 23 + 71
+// const kaolas = 65 + 54 + 49
+
+const calcAverage = (s1, s2, s3) => (s1 + s2 + s3) / 3;
+// console.log(calcAverage())
+
+// scoreDolphins = calcAverage(44 , 23 , 71)
+// scoreKaolas = calcAverage(65, 54, 49)
+// console.log(scoreDolphins, scoreKaolas)
+
+// const checkWinner = function(avgDolpins, avgKaolas){
+//   if (avgDolpins >= avgKaolas * 2){
+//     console.log(`Dolphins won with ${avgDolpins} vs ${avgKaolas} `)
+//   }else if (avgKaolas >= avgDolpins *2){
+//     console.log(`Kaolas won with ${avgKaolas} vs ${avgDolpins}`)
+//   }else {
+//     console.log('no one wins')
+//   }
+// }
+// checkWinner(scoreDolphins, scoreKaolas)
+// checkWinner(600, 100)
+
+// let num = Math.trunc(Math.random() * 6) + 1
+// console.log(num)
+
+// while (num !== 6) {
+//   console.log(`you rolled a ${num}`)
+//   num = Math.trunc(Math.random() * 6) + 1
+//   if(num === 6){
+//     console.log(`You have rolled ${num} so the loop will stop `)
+//   }
+// }
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+for (let i = 0; i < bills.length; i++) {
+  const element = bills[i];
+  console.log(element);
+  const tip = calcTip(bills[i]);
+  tips.push(element);
+  totals.push(element + bills[i]);
+}
+console.log(bills, tips, totals);
